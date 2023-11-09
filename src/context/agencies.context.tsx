907ccc280242f6623bf5402573a7fc1ca55af0ce
Cloé <61829801+cgslbs@users.noreply.github.com>
2023-11-09 11:54:12 +0100

    feat: :sparkles: Add AgencyContext
    - the AgencyContext will defined the selected agency by the user ( needed to apply bonus on some items )

---
+++ src/context/agencies.context.tsx
@@ -0,0 +1,57 @@
+'use client';
+import { AgenciesEnum } from '@/constants';
+import {
+  Dispatch,
+  ReactNode,
+  createContext,
+  useContext,
+  useMemo,
+  useReducer,
+  useState,
+} from 'react';
+import { User } from '../interfaces/index';
+
+type StateType = {
+  defaultAgency: AgenciesEnum | null;
+};
+
+type ActionType = AgenciesEnum;
+
+const initialState: StateType = {
+  defaultAgency: null,
+};
+
+const reducer = (state: User, action: ActionType) => {
+  if (action !== null) {
+    return { ...state, defaultAgency: action };
+  }
+  return { ...state, defaultAgency: null };
+};
+
+type AgencyContextType = {
+  state: StateType;
+  dispatch: Dispatch<AgenciesEnum>;
+};
+
+export const AgencyContext = createContext<AgencyContextType>({
+  state: initialState,
+  dispatch: () => null,
+});
+
+type GroupProviderProps = {
+  children: ReactNode;
+};
+
+export function AgencyContextProvider({ children }: GroupProviderProps) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AgencyContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AgencyContext.Provider>
+  );
+}
+
+export function useAgencyContext() {
+  return useContext(AgencyContext);
+}
