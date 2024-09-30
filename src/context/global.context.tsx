b57139b62fb3af68ab766832f7650bd593e3ae80
Cloé <61829801+cgslbs@users.noreply.github.com>
2024-09-30 13:27:53 +0200

    refactor: :fire: Remove Store context since its handled by the back-end now
    #12

--- src/context/global.context.tsx
+++ src/context/global.context.tsx
@@ -1,14 +1,11 @@
 import { ReactNode } from 'react'
 import { AgencyContextProvider } from './agencies.context'
 import { CartContextProvider } from './cart.context'
-import { StoreContextProvider } from './store.context'
 
 export default function GlobalContext({ children }: { children: ReactNode }) {
 	return (
 		<AgencyContextProvider>
-			<StoreContextProvider>
-				<CartContextProvider>{children}</CartContextProvider>
-			</StoreContextProvider>
+			<CartContextProvider>{children}</CartContextProvider>
 		</AgencyContextProvider>
 	)
 }
