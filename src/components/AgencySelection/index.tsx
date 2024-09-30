25c972265b898ed415d3a1f0a0e5d15fef3baf0b
Cloé <61829801+cgslbs@users.noreply.github.com>
2024-09-30 13:26:46 +0200

    refactor: :recycle: Update AgencyContext according to constant file + save agency code in localStorage
    #12

--- src/components/AgencySelection/index.tsx
+++ src/components/AgencySelection/index.tsx
@@ -1,19 +1,28 @@
 'use client'
 
-import { useEffect } from 'react'
 import { useAgency } from './AgencySelection.queries'
-import { ActionIcon, Box, Grid, Group, Modal, Stack, Text, Title, UnstyledButton } from '@mantine/core'
-import { AGENCY_ICON_KEYS } from './AgencySelection.constants'
+import { ActionIcon, Grid, Modal, Stack, Text, UnstyledButton } from '@mantine/core'
 import { IconDoor } from '@tabler/icons-react'
+import { useContext } from 'react'
+import { AgencyContext } from '@/context/agencies.context'
+import { AGENCY_ICON_KEYS, AgencyCodeEnum } from '@/constants/agency'
+import { useRouter } from 'next/navigation'
 
 export default function AgencySelection() {
-	const { data, status } = useAgency()
+	const { dispatch } = useContext(AgencyContext)
+	const { data } = useAgency()
+	const router = useRouter()
 
+	const onAgencySelection = (agencyCode: string) => {
+		dispatch(agencyCode as AgencyCodeEnum)
+		router.push('/store')
+	}
+
+	// TODO: create an error component
 	if (data === undefined) return <div>Something wrong happened oops</div>
 
 	return (
 		<Modal
-			// title='Choisissez votre agence / groupe !'
 			opened={true}
 			onClose={() => {}}
 			closeOnEscape={false}
@@ -24,27 +33,29 @@ export default function AgencySelection() {
 				<Text fw={700} size={'1.25rem'}>
 					Choisissez votre agence / groupe !
 				</Text>
-				<Grid grow justify='center' align='stretch'>
+				<Grid grow justify='center' align='stretch' gutter={{ base: 'md' }}>
 					{data
 						.filter((agency) => agency.code !== 'HR')
 						.map((agency) => {
 							const currAgency = Object.entries(AGENCY_ICON_KEYS).find(([keys, _value]) => keys === agency.code)
 							const Icon = currAgency !== undefined ? currAgency[1].icon : IconDoor
 							return (
-								<Grid.Col span={4}>
-									<Stack gap={'0.5rem'} align='center' justify='center' w={'100%'} component={UnstyledButton}>
-										<ActionIcon
-											variant='filled'
-											size='xl'
-											radius='xl'
-											aria-label={agency.name}
-											color={currAgency?.[1].color}>
-											<Icon />
-										</ActionIcon>
-										<Text fw={500} ta={'center'}>
-											{agency.name}
-										</Text>
-									</Stack>
+								<Grid.Col key={agency.id} span={4}>
+									<UnstyledButton onClick={() => onAgencySelection(agency.code)} w={'100%'}>
+										<Stack gap={'0.5rem'} align='center' justify='center' w={'100%'} h={'100%'}>
+											<ActionIcon
+												variant='filled'
+												size='xl'
+												radius='xl'
+												aria-label={agency.name}
+												color={currAgency?.[1].color}>
+												<Icon />
+											</ActionIcon>
+											<Text fw={500} ta={'center'}>
+												{agency.name}
+											</Text>
+										</Stack>
+									</UnstyledButton>
 								</Grid.Col>
 							)
 						})}
