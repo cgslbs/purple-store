0596da5819b313697ac82ab53874c096894e260e
Cloé <61829801+cgslbs@users.noreply.github.com>
2024-10-03 18:50:44 +0200

    refactor: :wrench: (ReactQuery) Set refetchOnWindowFocus to false
    #12

--- src/utils/ReactQueryProvider/index.tsx
+++ src/utils/ReactQueryProvider/index.tsx
@@ -4,7 +4,16 @@ import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-	const [queryClient] = useState(() => new QueryClient())
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnWindowFocus: false, // default: true
+					},
+				},
+			})
+	)
 
 	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
