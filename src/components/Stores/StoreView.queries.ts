66c0295233d2eff38e4eb80af8f703600dc19429
Cloé <61829801+cgslbs@users.noreply.github.com>
2024-10-03 18:54:24 +0200

    refactor: :zap: (Key) Add agencycode as key dependency for store api call
    #12

--- src/components/Stores/StoreView.queries.ts
+++ src/components/Stores/StoreView.queries.ts
@@ -2,5 +2,6 @@ import { fetchStoreByAgency } from '@/services/Store.service'
 import { useQuery } from 'react-query'
 
 export const useStoreByAgency = () => {
-	return useQuery(['storeByAgency'], () => fetchStoreByAgency())
+	const currAgencyCode = localStorage.getItem('agency_code')
+	return useQuery(['storeByAgency', currAgencyCode], () => fetchStoreByAgency(), { enabled: currAgencyCode !== null })
 }
